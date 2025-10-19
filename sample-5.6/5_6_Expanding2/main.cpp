c611b006801ddf36259f99ec6d888eb930e43f71
Maydyk <maydyk@gmail.com>
2025-10-19 13:58:45 +0300

    Samples 5.6.

---
+++ sample-5.6/5_6_Expanding2/main.cpp
@@ -0,0 +1,31 @@
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QSizePolicy>
+
+int main(int argc, char *argv[])
+{
+   QApplication app(argc, argv);
+   QWidget window;
+   window.setWindowTitle("Expanding");
+   window.resize(350, 150);
+
+   QLabel *label = new QLabel("Текст надписи");
+   QLabel *label2 = new QLabel("Текст надписи");
+
+   QSizePolicy policy(QSizePolicy::Expanding,
+                      QSizePolicy::Expanding);
+   label->setFrameStyle(QFrame::Box | QFrame::Plain);
+   label->setSizePolicy(policy);
+
+   QVBoxLayout *vbox = new QVBoxLayout();
+   vbox->addWidget(label);
+   vbox->addWidget(label2);
+   window.setLayout(vbox);
+
+   window.show();
+   return app.exec();
+}
+
---
+++ sample-5.6/5_6_Minimum/main.cpp
@@ -0,0 +1,32 @@
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QSizePolicy>
+
+int main(int argc, char *argv[])
+{
+   QApplication app(argc, argv);
+   QWidget window;
+   window.setWindowTitle("Minimum");
+   window.resize(350, 150);
+
+   QLabel *label = new QLabel("Текст надписи");
+   QPushButton *btn = new QPushButton("Текст на кнопке");
+
+   QSizePolicy policy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+   label->setFrameStyle(QFrame::Box | QFrame::Plain);
+   label->setSizePolicy(policy);
+
+   QVBoxLayout *vbox = new QVBoxLayout();
+   vbox->addWidget(label);
+   vbox->addWidget(btn);
+   window.setLayout(vbox);
+
+   window.show();
+
+
+   return app.exec();
+}
+
