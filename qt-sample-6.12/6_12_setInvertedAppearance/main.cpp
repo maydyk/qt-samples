63afcab942499a3491a0492b28cff8aecf2c59f4
Maydyk <maydyk@gmail.com>
2025-11-28 17:16:21 +0300

    Sample 6.12.

---
+++ qt-sample-6.12/6_12_setInvertedAppearance/main.cpp
@@ -0,0 +1,33 @@
+#include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QProgressBar>
+
+int main(int argc, char *argv[])
+{
+   QApplication app(argc, argv);
+   QWidget window;
+   window.setWindowTitle("Класс QProgressBar");
+   window.resize(350, 100);
+
+   QProgressBar *progressBar = new QProgressBar();
+   progressBar->setRange(0, 100);
+   progressBar->setValue(70);
+   progressBar->setInvertedAppearance(true);
+
+   QPushButton *btn1 = new QPushButton("Вывести значение");
+
+   QVBoxLayout *vbox = new QVBoxLayout();
+   vbox->addWidget(progressBar);
+   vbox->addWidget(btn1);
+   window.setLayout(vbox);
+
+   QObject::connect(btn1, &QPushButton::clicked, [=]() {
+      qDebug() << progressBar->value();
+   });
+
+   window.show();
+   return app.exec();
+}
+
