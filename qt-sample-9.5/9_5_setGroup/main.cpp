30844a8f4777f28ac9b1f3cd99a628b0fe2c3dce
Maydyk <maydyk@gmail.com>
2025-11-30 11:52:33 +0300

    Samples 9.5.

---
+++ qt-sample-9.5/9_5_setGroup/main.cpp
@@ -0,0 +1,52 @@
+#include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QGraphicsScene>
+#include <QGraphicsLineItem>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+#include <QGraphicsItemGroup>
+
+int main(int argc, char *argv[])
+{
+   QApplication app(argc, argv);
+   QWidget window;
+   window.setWindowTitle("Класс QGraphicsItemGroup");
+   window.resize(600, 400);
+
+   QGraphicsScene *scene = new QGraphicsScene(0.0, 0.0, 500.0, 335.0);
+   scene->setBackgroundBrush(Qt::white);
+
+   QGraphicsLineItem *line1 = scene->addLine(50.0, 50.0, 450.0, 50.0,
+                                             QPen(Qt::red, 3));
+   line1->setFlag(QGraphicsItem::ItemIsMovable);
+   line1->setFlag(QGraphicsItem::ItemIsSelectable);
+
+   QGraphicsLineItem *line2 = scene->addLine(QLineF(50.0, 100.0, 450.0, 100.0),
+                                             QPen(Qt::blue, 3));
+   line2->setFlag(QGraphicsItem::ItemIsMovable);
+   line2->setFlag(QGraphicsItem::ItemIsSelectable);
+
+   QGraphicsItemGroup *group = new QGraphicsItemGroup();
+   line1->setGroup(group);
+   line2->setGroup(group);
+   group->setFlag(QGraphicsItem::ItemIsMovable);
+   group->setFlag(QGraphicsItem::ItemIsSelectable);
+   scene->addItem(group);
+
+   QGraphicsRectItem *rect = scene->addRect(QRectF(0.0, 0.0, 400.0, 100.0),
+                        QPen(Qt::blue, 3),
+                        QBrush(Qt::green));
+   rect->setPos(QPointF(50.0, 150.0));
+   rect->setFlag(QGraphicsItem::ItemIsMovable);
+   rect->setFlag(QGraphicsItem::ItemIsSelectable);
+
+   QGraphicsView *view = new QGraphicsView(scene);
+
+   QVBoxLayout *vbox = new QVBoxLayout();
+   vbox->addWidget(view);
+   window.setLayout(vbox);
+
+   window.show();
+   return app.exec();
+}
